e3bb47f10b86583b82e151dc46312ba1f0b34ed0
Trinity7509 <iren75iv@gmail.com>
2018-12-20 12:16:17 +0000

    commit

---
+++ CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator_Csharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public IActionResult Calculate (Calculator calculator)
+        {
+            calculator.CalculateResult();
+            return RedirectToAction("Index", calculator);
+        }
+
+    }
+}
